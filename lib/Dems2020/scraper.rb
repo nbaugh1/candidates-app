08d19dbfe412a504a2541099779fe4c279dec84c
nbaugh1 <nbaugh1@gmail.com>
2019-07-09 10:49:37 -0400

    scrape method now grabs candidate name and profile url, places them in a hash, adds hashes to an array

--- lib/Dems2020/scraper.rb
+++ lib/Dems2020/scraper.rb
@@ -1,18 +1,17 @@
 class Dems2020::Scraper
+    
     BASE_URL = "https://ballotpedia.org"
     LIST_URL = "#{BASE_URL}/Democratic_presidential_nomination,_2020"
+    
     def self.scrape
         doc = Nokogiri::HTML(open(LIST_URL))
-        #binding.pry
-        cand_list = []
-        cand_links = []
-        
-        doc.css("div.mobile-columns ul li").each do |name|           #GRABS CANDIDATES NAMES
-            cand_list << name.text
+        candidates = []
+        doc.css("div.mobile-columns ul li b a").each do |candidate|           
+            candidates << {name: candidate.text, profile_URL: candidate.attribute('href').value}
         end
-        doc.css("div.mobile-columns ul li b a").each do |link|       #GRABS URL SLUGS FOR PROFILE PAGES
-            cand_links << link.attribute('href').value
-        end
-        #binding.pry
+    end
+
+    def self.profile_scraper (profile_url)
+    
     end
 end
