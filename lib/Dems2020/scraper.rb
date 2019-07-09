cf643551bffdd99511381c7272a96c4ae2fe0a8a
nbaugh1 <nbaugh1@gmail.com>
2019-07-09 10:04:01 -0400

    scraper now grabs candidate profile page urls

--- lib/Dems2020/scraper.rb
+++ lib/Dems2020/scraper.rb
@@ -1,10 +1,18 @@
 class Dems2020::Scraper
+    BASE_URL = "https://ballotpedia.org"
+    LIST_URL = "#{BASE_URL}/Democratic_presidential_nomination,_2020"
     def self.scrape
-        doc = Nokogiri::HTML(open("https://ballotpedia.org/Democratic_presidential_nomination,_2020"))
+        doc = Nokogiri::HTML(open(LIST_URL))
+        #binding.pry
         cand_list = []
-        doc.css("div.mobile-columns ul li b a").each do |name|
+        cand_links = []
+        
+        doc.css("div.mobile-columns ul li").each do |name|           #GRABS CANDIDATES NAMES
             cand_list << name.text
         end
-        binding.pry
+        doc.css("div.mobile-columns ul li b a").each do |link|       #GRABS URL SLUGS FOR PROFILE PAGES
+            cand_links << link.attribute('href').value
+        end
+        #binding.pry
     end
 end
